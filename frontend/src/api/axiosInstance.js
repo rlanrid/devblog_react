da6d2b5819f60e24ec556703a7fa8f8db76f40a3
rlan <rladnrider77@gmail.com>
2026-04-23 22:35:12 +0900

    update

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
 const axiosInstance = axios.create({
-  baseURL: "https://devblog-react-xv6c.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const public__paths = ["/login", "/register", "/password"];
